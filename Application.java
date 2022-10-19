9317f38628e7d682ba6721789f9499bbf076c5bc
Yassine mathlouthi <84955294+screen-T@users.noreply.github.com>
2022-10-19 23:50:21 +0100

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,53 @@
+package tp3;
+
+public class Application {
+
+	public static void main(String[] args)
+	{ /*
+		Compte c1= new Compte(120.5) ;
+		Compte c2= new Compte(1850.5) ;
+		System.out.println(c1.affiche()) ;
+		System.out.println(c2.affiche()) ;
+		c2.verser(200);
+		System.out.println(c2.affiche()) ;
+		if (c1.retire(200)==true)
+		{
+			System.out.println("réussit"+c1.affiche()) ;
+		}
+		else 
+		{
+			System.out.println("non votre max est "+c1.getSolde()) ; ;
+		}
+		*/
+	    boolean teste ;  
+		Compte c3= new Compte(70) ;
+		Compte c4= new Compte(70) ;
+		c3=OperationsBancaires.creeCompte(20) ;
+		System.out.println(c3.affiche()) ;
+		teste=OperationsBancaires.ajoutCompte(c3) ;
+		if (teste==true)
+		{
+			System.out.println("succesfull aded") ;
+		}
+		else
+		{
+			System.out.println("failed aded") ;
+			
+		}
+		c4=OperationsBancaires.creeCompte(50) ;
+		System.out.println(c4.affiche()) ;
+		teste=OperationsBancaires.ajoutCompte(c4) ;
+		if (teste==true)
+		{
+			System.out.println("succesfull aded") ;
+		}
+		else
+		{
+			System.out.println("failed aded") ;
+			
+		}
+		
+		
+	}
+		
+}
